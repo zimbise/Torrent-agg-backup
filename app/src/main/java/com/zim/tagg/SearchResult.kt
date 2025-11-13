b378571298c93fc94afef7126f2cf7a4d388a37e
Zim <zimbise@gmail.com>
2025-11-12 23:25:39 -0500

    Update and rename app/src/main/java/com/zim/tagg/SearchResult.kt to Y

--- app/src/main/java/com/zim/tagg/SearchResult.kt
+++
@@ -1,9 +0,0 @@
-package com.zim.tagg
-
-data class SearchResult(
-    val title: String,
-    val magnet: String,
-    val size: String? = null,
-    val seeds: Int? = null,
-    val provider: String? = null
-)
